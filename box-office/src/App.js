7710953a26cd969fe9ba6b5025f10460a97c01d1
vidhya <vidhu82@gmail.com>
2021-09-24 19:23:04 +0530

    added layout management and created title

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Navs';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 function App() {
   // exact matches the exact path, <Route></Route> is to handle the undefined routes
   return (
-    <div>
-      <Navs />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route>
-          <div>Not found</div>
-        </Route>
-      </Switch>
-    </div>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
   );
 }
 
