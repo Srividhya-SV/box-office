037dc4e2638295bf7b2c7ce7c2e84aeabb994cbb
vidhya <vidhu82@gmail.com>
2021-09-24 16:04:40 +0530

    Added react router

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hi</div>;
+  // exact matches the exact path, <Route></Route> is to handle the undefined routes
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/login">
+        This is login page
+      </Route>
+      <Route>This is 404 page</Route>
+    </Switch>
+  );
 }
 
 export default App;
