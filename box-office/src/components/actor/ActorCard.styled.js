9db5110b2407d7aca1a61eeeed1ca9bd4c1ceda8
vidhya <vidhu82@gmail.com>
2021-09-28 17:43:26 +0530

    installed styled components and created styles for search cards

---
+++ box-office/src/components/actor/ActorCard.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { SearchCard } from '../styled';
+
+// DEsign will be on top of the searchcard div
+export const StyledActorCard = styled(SearchCard)`
+  .deathday {
+    margin: 0;
+    margin-top: 15px;
+    font-weight: bold;
+  }
+`;
