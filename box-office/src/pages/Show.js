66abfebaaa3b39a6d9967af34a7e16def6148af6
vidhya <vidhu82@gmail.com>
2021-09-29 07:26:01 +0530

    Added loading and error messages to the show page

--- box-office/src/pages/Show.js
+++ box-office/src/pages/Show.js
@@ -6,15 +6,38 @@ const Show = () => {
   const { id } = useParams();
 
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   // As the id is static we can use that in dependancies array
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    // As the state needs to be updated only when ths component is mounted
+    const isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
   }, [id]);
 
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error occurred: {error}</div>;
+  }
   return <div>This is show page</div>;
 };
 
